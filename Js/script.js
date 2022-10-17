51b3a5c28b21dba41646e5f4f7f1a4f5340e5d57
Tavaris Bellamy <tavaris.bellamy80@gmail.com>
2022-10-17 17:35:52 -0400

    added upercase character list and if else statements and is statements for character list to choose from

--- Js/script.js
+++ Js/script.js
@@ -26,11 +26,30 @@ var generateBtn = document.querySelector("#generate");
  var numberList = ["0","1","2","3","4","5","6","7","8","9"]
  var symbolList = ["!","@","#","$","&","*"]
  var lowercaseList =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
- var upercaseList =[]
+ var upercaseList =["A","B","C","D","E","F","G","H","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+ var charactersList = []
+ console.log(charactersList)
  }
 
+ if(userWantsNumbers === true) {
+  charactersList.push(numberList)
+ }
 
- 
+ if (userWantsSymbols === true){
+  charactersList.push(symbolList)
+ }
+
+  if (userWantsLowercase === true){
+  charactersList.push(lowercaseList)
+}
+
+if (userWantsUppercase === true){
+  charactersList.push(upercaseList)
+
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
